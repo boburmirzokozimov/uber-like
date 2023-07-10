5b7857b62621e2167cdeea9f24d33459cf83f717
boburmirzo <bobur.kozimov2302@gmail.com>
2023-07-10 17:54:27 +0500

    Doing front

---
+++ backend/app/Events/TripAccepted.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Trip;
+use App\Models\User;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class TripAccepted
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public function __construct(public Trip $trip,private readonly User $user)
+    {
+    }
+
+    public function broadcastOn(): array
+    {
+        return [
+            new Channel('passenger_'.$this->user->id),
+        ];
+    }
+}
--- backend/app/Http/Controllers/TripController.php
+++ backend/app/Http/Controllers/TripController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\TripAccepted;
+use App\Events\TripEnded;
+use App\Events\TripLocationUpdated;
+use App\Events\TripStarted;
 use App\Models\Trip;
 use Illuminate\Http\Request;
 
@@ -50,16 +54,20 @@ class TripController extends Controller
 
         $trip->load('driver.user');
 
+        TripAccepted::dispatch($trip,$request->user());
+
         return $trip;
     }
 
-    public function start(Trip $trip)
+    public function start(Request $request,Trip $trip)
     {
         $trip->update([
             'is_started' => true
         ]);
 
         $trip->load('driver.user');
+
+        TripStarted::dispatch($trip,$request->user());
 
         return $trip;
     }
@@ -72,6 +80,8 @@ class TripController extends Controller
 
         $trip->load('driver.user');
 
+        TripEnded::dispatch($trip,$request->user());
+
         return $trip;
     }
 
@@ -82,6 +92,8 @@ class TripController extends Controller
         $trip->update(['driver_location' => $request->driver_location]);
 
         $trip->load('driver.user');
+
+        TripLocationUpdated::dispatch($trip,$request->user());
 
         return $trip;
     }
